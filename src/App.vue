d1040e2d985c5f6b7922c5dc2f4fedcd910b6449
kotnikd3 <denis.kotnik@gmail.com>
2026-06-05 15:51:58 +0200

    feat: add logo to the footer

--- src/App.vue
+++ src/App.vue
@@ -105,7 +105,12 @@
                     </ul>
                 </div>
             </div>
-            <div class="has-text-centered pt-5 has-text-primary">
+            <div class="columns is-centered is-mobile">
+                <div class="column is-3-mobile is-2-tablet is-1-desktop">
+                    <img src="@/assets/images/logo.webp" alt="psychotherapy_kotnik_logo">
+                </div>
+            </div>
+            <div class="has-text-centered has-text-primary">
                 <p><span>Psychotherapy in Leeuwarden for Expats | Psychotherapy Kotnik</span> <font-awesome-icon icon="fa-regular fa-copyright" /> 2026</p>
 			</div>
         </div>
